a97460a9d718f25441f386dfca567f81bea3613a
maidung1602 <maidung1602@gmail.com>
2024-09-26 04:09:11 +0700

    feat: token service

---
+++ be/src/services/Token.service.js
@@ -0,0 +1,22 @@
+import jwt from 'jsonwebtoken'
+import * as dotenv from 'dotenv'
+dotenv.config();
+
+const { REFRESH_KEY, ACCESS_KEY } = process.env;
+
+class Token {
+    async genAccessToken(account) {
+        return jwt.sign({
+            id: account._id,
+            accountType: account.accountType,
+        }, ACCESS_KEY, { expiresIn: "10h" });
+    }
+    async genRefreshToken(account) {
+        return jwt.sign({
+            id: account._id,
+            accountType: account.accountType,
+        }, REFRESH_KEY, { expiresIn: "365d" });
+    }
+}
+
+export default new Token()
