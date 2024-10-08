e6b242c30d607be2d17ed12290faf7969364c3f8
maidung1602 <maidung1602@gmail.com>
2024-10-08 09:59:04 +0700

    feat: crud for default price

--- be/src/routes/index.route.js
+++ be/src/routes/index.route.js
@@ -5,6 +5,7 @@ import HouseRoute from "./House.router.js";
 import AccountRoute from "./Account.route.js";
 import {verifyToken} from "../middlewares/verifyToken.middleware.js";
 import newsRouter from "./news.router.js";
+import defaultPriceRoute from "./DefaultPrice.route.js";
 
 const indexRouter = express.Router();
 
@@ -12,5 +13,6 @@ indexRouter.use("/house",verifyToken,HouseRoute)
 indexRouter.use("/account",verifyToken, AccountRoute)
 indexRouter.use("/auth", AuthRoute);
 indexRouter.use("/news",verifyToken,newsRouter)
+indexRouter.use("/defaultPrice",defaultPriceRoute)
 
 export default indexRouter;
