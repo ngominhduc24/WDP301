e08b91eee0945b6a882a1222745ad8dc94a58019
ngominhduc24 <ngominhduc100@gmail.com>
2024-10-16 22:24:13 +0700

    controller statistic

--- be/src/controllers/Statistic.controller.js
+++ be/src/controllers/Statistic.controller.js
@@ -1,49 +1,76 @@
 import statisticsService from "../services/Statistics.service.js";
-import asyncHandler from "../utils/async-handler.js"
-import response from "../utils/response.js"
+import asyncHandler from "../utils/async-handler.js";
+import response from "../utils/response.js";
 
 const StatisticController = {
-    statisticGeneral: asyncHandler(async (req,res,next) => {
-        try {
-            const statistics = await statisticsService.statisticGeneral(req);
-            if (statistics) return res.status(200).json(response.successResponse(200,statistics))
-            else res.status(404).json(response.errorResponse(404));            
-        } catch (error) {
-            return res.status(500).json(response.errorResponse(500,error.toString()));
-        }
+  statisticGeneral: asyncHandler(async (req, res, next) => {
+    try {
+      const statistics = await statisticsService.statisticGeneral(req);
+      if (statistics)
+        return res.status(200).json(response.successResponse(200, statistics));
+      else res.status(404).json(response.errorResponse(404));
+    } catch (error) {
+      return res
+        .status(500)
+        .json(response.errorResponse(500, error.toString()));
+    }
+  }),
+  statisticAllBills: asyncHandler(async (req, res, next) => {
+    try {
+      const statistics = await statisticsService.statisticAllBills(req);
+      if (statistics)
+        return res.status(200).json(response.successResponse(200, statistics));
+      else res.status(404).json(response.errorResponse(404));
+    } catch (error) {
+      return res
+        .status(500)
+        .json(response.errorResponse(500, error.toString()));
+    }
+  }),
+  statisticProblem: asyncHandler(async (req, res, next) => {
+    try {
+      const statistics = await statisticsService.statisticProblem(req);
+      if (statistics)
+        return res.status(200).json(response.successResponse(200, statistics));
+      else res.status(404).json(response.errorResponse(404));
+    } catch (error) {
+      return res
+        .status(500)
+        .json(response.errorResponse(500, error.toString()));
+    }
+  }),
+  statisticRevenue: asyncHandler(async (req, res, next) => {
+    try {
+      const statistics = await statisticsService.statisticRevenue(req);
+      if (statistics)
+        return res.status(200).json(response.successResponse(200, statistics));
+      else res.status(404).json(response.errorResponse(404));
+    } catch (error) {
+      return res
+        .status(500)
+        .json(response.errorResponse(500, error.toString()));
+    }
+  }),
+  GetProductById: asyncHandler(async (req, res, next) => {
+    try {
+      const product = await ProductService.getProductById(req.params.id);
+      res.status(200).send(product);
+    } catch (error) {
+      next(error);
+    }
+  }),
 
-    }),
-    statisticAllBills: asyncHandler(async (req,res,next) => {
-        try {
-            const statistics = await statisticsService.statisticAllBills(req);
-            if (statistics) return res.status(200).json(response.successResponse(200,statistics))
-            else res.status(404).json(response.errorResponse(404));            
-        } catch (error) {
-            return res.status(500).json(response.errorResponse(500,error.toString()));
-        }
+  UpdateProduct: asyncHandler(async (req, res, next) => {
+    try {
+      const product = await ProductService.updateProduct(
+        req.params.id,
+        req.body
+      );
+      res.status(200).send(product);
+    } catch (error) {
+      next(error);
+    }
+  }),
+};
 
-    }),
-    statisticProblem: asyncHandler(async (req,res,next) => {
-        try {
-            const statistics = await statisticsService.statisticProblem(req);
-            if (statistics) return res.status(200).json(response.successResponse(200,statistics))
-            else res.status(404).json(response.errorResponse(404));            
-        } catch (error) {
-            return res.status(500).json(response.errorResponse(500,error.toString()));
-        }
-
-    }),
-    statisticRevenue: asyncHandler(async (req,res,next) => {
-        try {
-            const statistics = await statisticsService.statisticRevenue(req);
-            if (statistics) return res.status(200).json(response.successResponse(200,statistics))
-            else res.status(404).json(response.errorResponse(404));            
-        } catch (error) {
-            return res.status(500).json(response.errorResponse(500,error.toString()));
-        }
-
-    }),
-    
-}
-
-export default StatisticController
+export default StatisticController;
