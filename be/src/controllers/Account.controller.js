bb1b232026ce244a76d8c504e1637f279914814f
maidung1602 <maidung1602@gmail.com>
2024-10-22 03:21:00 +0700

    fix: Account controller

--- be/src/controllers/Account.controller.js
+++ be/src/controllers/Account.controller.js
@@ -2,6 +2,12 @@ import AccountService from "../services/Account.service.js";
 import asyncHandler from "../utils/async-handler.js";
 
 const AccountController = {
+  getAll: asyncHandler(async (req, res) => {
+    await AccountService.getAll(req, res);
+  }),
+  createAccount: asyncHandler(async (req, res) => {
+    await AccountService.createAccount(req, res);
+  }),
   getProfile: asyncHandler(async (req, res) => {
     await AccountService.getProfile(req, res);
   }),
