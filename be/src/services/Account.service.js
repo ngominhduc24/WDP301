e5990c87aea4b7de6bb1f0b8f8678134babf7d88
maidung1602 <maidung1602@gmail.com>
2024-10-22 08:22:22 +0700

    fix: account

--- be/src/services/Account.service.js
+++ be/src/services/Account.service.js
@@ -3,6 +3,82 @@ import bcrypt from "bcrypt";
 import getCurrentUser from "../utils/getCurrentUser.js";
 
 class AccountService {
+  async getAll(req, res) {
+    try {
+        const { page, limit } = req.query;
+        const pageNumber = parseInt(page) || 1;
+        const limitPerPage = parseInt(limit) || 10;
+        const skip = (pageNumber - 1) * limitPerPage;
+        
+        const { houseId } = req.params;
+
+        const rooms = await Room.find({ houseId});
+        const totalAccounts = await Account.countDocuments({ roomId: { $in: rooms.map((room) => room._id) } });
+        const data = await Account.find({ roomId: { $in: rooms.map((room) => room._id) } })
+                .skip(skip)
+                .limit(limitPerPage)
+                .sort({ createdAt: -1 })
+                .exec();
+
+        const totalPages = Math.ceil(totalAccounts / limitPerPage);
+
+        return res.status(201).json({
+            pagination: {
+                currentPage: pageNumber,
+                totalPages: totalPages,
+                totalAccounts: totalAccounts,
+                accountsPerPage: data.length,
+            },
+            data: data,
+        });
+    } catch (error) {
+        return res.status(500).json({
+            message: "Internal Server Error",
+        });
+    }
+}
+
+async createAccount(req, res) {
+    const { email,username, role, name } = req.body;
+    try {
+        const checkEmailExists = await Account.findOne({ email: email });
+        if (checkEmailExists !== null)
+            return res.status(400).json({ message: "Email has exists" });
+        const checkUsername = await Account.findOne({username})
+        if (checkUsername !== null) {
+            return res.status(400).json({ message: "Username has exists" });
+        }
+
+        // const nanoid = customAlphabet("1234567890ABCdef", 10);
+        // const password = '@Fpt1' + nanoid();
+        const password = "Admin@123";
+
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        const accountData = await Account.create({
+            username,
+            name,
+            email,
+            password: hashedPassword,
+            accountType: role,
+        });
+
+        return res.status(201).json({
+            message: "Create Successfully",
+            data: {
+                username: accountData.username,
+                name: accountData.name,
+                email: accountData.email,
+                accountType: accountData.accountType,
+            },
+        });
+    } catch (error) {
+        return res.status(500).json({
+            message: "Internal Server Error",
+        });
+    }
+}
   async getProfile(req, res) {
     try {
       const accountId = getCurrentUser(req);
